83b309032347c400c56f322b5666a41a18c3f1e8
Edwin Sneider Velandia Suarez <ivinernesto123@gmail.com>
2024-05-17 22:53:20 -0500

    avances

--- src/config/global.js
+++ src/config/global.js
@@ -1,17 +1,23 @@
 export default {
   global: {
-    componenteFormativo: 'NOMBRE DEL COMPONENTE FORMATIVO',
-    descripcionCurso: 'BREVE DESCRIPCIÓN',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.svg'),
+    componenteFormativo:
+      'Diseño y herramientas para la construcción de contenido digital',
+    descripcionCurso:
+      'En este componente se abordarán los diferentes procesos y las herramientas necesarias para la construcción de una imagen, presentaciones, audios y videos.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.png'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
       {
         clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.svg'),
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.png'),
       },
       {
         clases: ['banner-principal-decorativo-2'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.png'),
+      },
+      {
+        clases: ['banner-principal-decorativo-3'],
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-3.png'),
       },
     ],
   },
@@ -31,27 +37,34 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Diseño y concepto de piezas gráficas',
         desarrolloContenidos: true,
-        subMenu: [
-          {
-            numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
-            hash: 't_1_1',
-          },
-        ],
       },
-
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Herramientas para la construcción de una imagen',
         desarrolloContenidos: true,
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Titulo de primer nivel',
+        titulo:
+          'Planeación, diseño y herramientas para elaborar presentaciones',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema4',
+        numero: '4',
+        titulo:
+          'Planeación, diseño y herramientas para la creación de un audio',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema5',
+        numero: '5',
+        titulo:
+          'Planeación, diseño y herramientas para la creación de un video',
         desarrolloContenidos: true,
       },
     ],
@@ -102,32 +115,150 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: 'Planeación, diseño y herramientas para la creación de un audio',
+      referencia:
+        'VIRTUOSSO.COM. (2018). <em>Ejercicios para locutores, ejercicios de locución y vibración</em> (video). YouTube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=JE3Yn4-6znE',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: 'Planeación, diseño y herramientas para la creación de un audio',
+      referencia:
+        'Martínez, A. (2019). <em>Cuatro tips para grabar tu voz… si no eres profesional de la locución</em>. Subitus.',
+      tipo: 'Artículo',
+      link: 'https://www.subitus.com/tips-basicos-para-grabar-voz/',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Texto de la definición',
+      termino: 'Amplificar',
+      significado:
+        'Este efecto incrementa o reduce el volumen del audio que se ha seleccionado.',
     },
     {
-      termino: '<em>Término con extranjerismo</em>',
-      significado: 'Definición',
+      termino: '<em>Auto Duck</em>',
+      significado:
+        'Reduce el volumen de una o más pistas cuando el volumen de la pista “control” alcanza un nivel particular. Típicamente usado para hacer que una pista de música suene más despacio cuando se escucha voz en una pista de comentarios.',
+    },
+    {
+      termino: 'Autoplay',
+      significado: 'Es un video que se reproduce automáticamente.',
+    },
+    {
+      termino: '<em>Backup</em>',
+      significado:
+        'Copia de seguridad de archivos, que se crea para evitar que se pierda la información gráfica en caso que se pierdan los originales, o se necesite hacer nuevos cambios en el diseño.',
+    },
+    {
+      termino: '<em>Bit Rate</em>',
+      significado:
+        'Se refiere a la cantidad de información por segundo que nuestro ordenador o móvil lee al reproducir un archivo de video. Por tanto, cuanto mayor sea el bitrate mayor será la calidad del material.',
+    },
+    {
+      termino: 'Canva',
+      significado:
+        'Lienzo donde se diseñan las piezas gráficas en los programas de diseño.',
+    },
+    {
+      termino: '<em>Embed</em>',
+      significado:
+        'Es incrustar un video que ha sido subido a una plataforma, en una web o en un blog.',
+    },
+    {
+      termino: 'Filtro',
+      significado:
+        'Capas de color que permiten cambiar la intensidad de nuestras fotografías.',
+    },
+    {
+      termino: 'Formato de imagen',
+      significado:
+        'Datos digitales en un formato específico que permiten ser visualizados en una computadora o dispositivo compatible; los más usados son: PNG, JPEG, TIFF y GIF.',
+    },
+    {
+      termino: 'Lecturabilidad',
+      significado:
+        'Cualidad de un diseño que permite leer con facilidad el texto que hay en él.',
+    },
+    {
+      termino: 'Legibilidad',
+      significado: 'Cualidad que tiene un texto para leerse sin dificultad.',
+    },
+    {
+      termino: 'M1V',
+      significado:
+        'Extensión de un archivo MPEG que solo contiene datos de video. MPA, MPEG, MPG.',
+    },
+    {
+      termino: 'M4A',
+      significado:
+        'Es un contenedor para distribuir ficheros de audio codificados en formato AAC o ALAC protegidos mediante DRM.',
+    },
+    {
+      termino: 'Mapa de Bits (Bitmaps)',
+      significado:
+        'Hace referencia a una imagen que está constituida por pixeles que tienen información de color. Los formatos de imagen para mapa de bits son: PNG, JPEG y GIF. El programa más usado para editar estas imágenes es Photoshop.',
+    },
+    {
+      termino: 'Multimedia',
+      significado:
+        'El término se refiere a cualquier plataforma que utiliza diferentes medios de expresión para comunicar y dar a conocer información, desde texto e imágenes hasta gifs, videos y audios',
+    },
+    {
+      termino: 'Pixel',
+      significado:
+        'Elemento más pequeño de la imagen de un monitor. El término proviene de dos palabras inglesas: picture y element.',
+    },
+    {
+      termino: 'Pódcast',
+      significado:
+        'Publicación digital en audio o video que se puede descargar o escuchar vía streaming. Suelen alojarse en una página web o blog, pero también en plataformas especializadas en audio como iTunes, Spotify o SoundCloud.',
+    },
+    {
+      termino: 'Resolución',
+      significado:
+        'Cantidad de píxeles que pueden visualizarse en el monitor horizontal y verticalmente. Cuanto más alta es la resolución, más detalles pueden visualizarse. ',
+    },
+    {
+      termino: 'RGB',
+      significado:
+        'Rojo, verde y azul. Colores primarios de la mezcla aditiva de colores. RGB designa el método que se emplea en informática para codificar la información de la imagen en pixeles, cada uno con una determinada combinación de estos tres colores primarios.',
+    },
+    {
+      termino: 'Playlist',
+      significado:
+        'Es una lista de videos online, organizados por temas o categorías. Muy populares en YouTube.',
+    },
+    {
+      termino: 'Video 360',
+      significado:
+        'Son videos grabados con cámaras especiales, capaces de captar la acción en un ángulo de 360 grados.',
+    },
+    {
+      termino: '<em>Voz en off</em>',
+      significado:
+        'Narración en la que el locutor no está en la escena. Se suele añadir al editar.',
     },
   ],
   referencias: [
     {
-      referencia: '',
-      link: '',
+      referencia:
+        '33mm. (2019). Diez consejos sobre cómo editar vídeos de forma profesional. ',
+      link:
+        'https://35mm.es/como-editar-videos-profesionalmente-10-consejos-utiles/',
+    },
+    {
+      referencia: 'Caño Valls, C. (2010). Tutoriales Audacity',
+      link: 'http://openaccess.uoc.edu/webapps/o2/handle/10609/9047',
+    },
+    {
+      referencia:
+        'Luciano Baêta. (2019). Conoce los principales conceptos y consejos para editar audio. ',
+      link: 'https://blog.hotmart.com/es/editar-audio/',
+    },
+    {
+      referencia:
+        'Mejía, M. (2021). ¡Dale vida a tu producto digital con Figma!',
+      link: 'https://www.crehana.com/co/blog/diseno-grafico/que-es-figma/',
     },
   ],
   creditos: [
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -9,6 +9,38 @@
           i.fas.fa-info
       h1 Introducción
     
+    .row.justify-content-center(data-aos="fade-left")
+      .col-lg-10
+        .bloque-texto-a.fondo-color-sistema-i.p-4.p-md-5.mb-5 
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              h2.mb-0 ¿Recuerdas que contenido digital es toda la información que se muestra en un medio digital?
+            .col-lg-8
+              .bloque-texto-a__texto.p-4
+                p Esa información la encontramos en páginas web, en redes sociales o en cualquier rincón de Internet, y está conformada por imágenes, textos, videos, y muchos elementos más.
+                  br
+                  br
+                  | En este componente formativo conoceremos cómo diseñar piezas gráficas y cuáles herramientas podemos utilizar para su elaboración.
+
+    .row.align-items-center.justify-content-center(data-aos="fade-right")
+      .col-8.col-md-4.mb-4.mb-lg-0
+        figure
+          img(src="@/assets/curso/temas/intro-img.svg", alt="imagen decorativa relacionada a computacion, documentos y medios digitales")
+        
+      .col-md
+        p.mb-5 También planearemos, diseñaremos y conoceremos las herramientas para elaborar presentaciones y audios, y por supuesto, no podemos olvidarnos de la planeación, el diseño y herramientas para la creación de un video. 
+          br
+          br
+          | Todos estos contenidos nos servirán para publicar en cualquier medio digital, porque hay que tener en cuenta que: 
+          b el contenido de calidad es uno de los pilares esenciales de cualquier proyecto digital.
+
+        .row
+          .col-md-8
+            .cajon.fondo-color-sistema-j.p-4
+              p.mb-0 Recuerda: 
+                b ¡la creatividad no tiene límites! 
+                | ¡Muchos éxitos en este aprendizaje!
+    
 </template>
 
 <script>
@@ -20,4 +52,7 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass" scoped>
+.bloque-texto-a:before
+  background-color: $color-primario
+</style>
